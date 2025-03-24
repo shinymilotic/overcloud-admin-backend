ff17b00c0db17ed2611ed6db5456c5a47e1a5a05
shinymilotic <trungtin.mai1412@gmail.com>
2025-03-24 14:21:44 +0700

    update

--- src/main/java/overcloud/blog/usecase/article/delete_article/DeleteArticleImpl.java
+++ src/main/java/overcloud/blog/usecase/article/delete_article/DeleteArticleImpl.java
@@ -1,4 +1,39 @@
 package overcloud.blog.usecase.article.delete_article;
 
-public class DeleteArticleImpl {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import overcloud.blog.repository.ArticleRepository;
+import overcloud.blog.repository.ArticleTagRepository;
+import overcloud.blog.repository.CommentRepository;
+import overcloud.blog.repository.FavoriteRepository;
+import java.util.UUID;
+
+@Service
+public class DeleteArticleImpl implements DeleteArticle {
+    private final ArticleRepository articleRepository;
+    private final ArticleTagRepository articleTagRepository;
+    private final CommentRepository commentRepository;
+    private final FavoriteRepository favoriteRepository;
+
+    public DeleteArticleImpl(ArticleRepository articleRepository,
+                             ArticleTagRepository articleTagRepository,
+                             CommentRepository commentRepository,
+                             FavoriteRepository favoriteRepository) {
+        this.articleRepository = articleRepository;
+        this.articleTagRepository = articleTagRepository;
+        this.commentRepository = commentRepository;
+        this.favoriteRepository = favoriteRepository;
+    }
+
+    @Override
+    @Transactional
+    public Void deleteArticle(String articleId) {
+        UUID uuidArticleId = UUID.fromString(articleId);
+        commentRepository.deleteByArticleId(uuidArticleId);
+        favoriteRepository.deleteByArticleId(uuidArticleId);
+        articleTagRepository.deleteByArticleId(uuidArticleId);
+        articleRepository.deleteById(uuidArticleId);
+        articleRepository.updateSearchVector();
+
+        return null;    }
 }

81ce20b3d0655fd3efa3c3df07a67e0012a52f74
shinymilotic <trungtin.mai1412@gmail.com>
2025-03-24 12:26:19 +0700

    update

--- src/main/java/overcloud/blog/usecase/article/update_article/UpdateArticleImpl.java
+++ src/main/java/overcloud/blog/usecase/article/update_article/UpdateArticleImpl.java
@@ -1,4 +1,36 @@
 package overcloud.blog.usecase.article.update_article;
 
-public class UpdateArticleImpl {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import overcloud.blog.auth.service.SpringAuthenticationService;
+import overcloud.blog.repository.ArticleRepository;
+import overcloud.blog.repository.ArticleTagRepository;
+import overcloud.blog.repository.TagRepository;
+import overcloud.blog.utils.validation.ObjectsValidator;
+
+@Service
+public class UpdateArticleImpl implements UpdateArticle {
+    private final SpringAuthenticationService authenticationService;
+    private final ArticleRepository articleRepository;
+    private final TagRepository tagRepository;
+    private final ArticleTagRepository articleTagRepository;
+    private final ObjectsValidator<UpdateArticleRequest> validator;
+
+    public UpdateArticleImpl(SpringAuthenticationService authenticationService,
+                             ArticleRepository articleRepository,
+                             TagRepository tagRepository,
+                             ArticleTagRepository articleTagRepository,
+                             ObjectsValidator<UpdateArticleRequest> validator) {
+        this.authenticationService = authenticationService;
+        this.articleRepository = articleRepository;
+        this.tagRepository = tagRepository;
+        this.articleTagRepository = articleTagRepository;
+        this.validator = validator;
+    }
+
+    @Override
+    @Transactional
+    public Void updateArticle(String articleId, UpdateArticleRequest request) {
+        return null;
+    }
 }
--- src/main/java/overcloud/blog/usecase/article/update_article/UpdateArticleRequest.java
+++ src/main/java/overcloud/blog/usecase/article/update_article/UpdateArticleRequest.java
@@ -1,4 +1,32 @@
 package overcloud.blog.usecase.article.update_article;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateArticleRequest {
+    @JsonProperty("title")
+    @Size(min = 1, max = 60, message = "article.update.title.size")
+    private String title;
+
+    @JsonProperty("description")
+    @NotBlank(message = "article.update.description.not-blank")
+    @Size(min = 1, max = 100, message = "article.update.description.size")
+    private String description;
+
+    @JsonProperty("body")
+    @NotBlank(message = "article.update.body.not-blank")
+    private String body;
+
+    @JsonProperty("tagList")
+    @NotEmpty(message = "article.update.tagList.not-empty")
+    private List<String> tagList;
 }

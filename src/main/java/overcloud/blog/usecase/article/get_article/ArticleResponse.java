6262f0d7652a4e9ad6877ac18d733ad183d94ded
shinymilotic <trungtin.mai1412@gmail.com>
2025-03-25 20:46:55 +0700

    update

--- src/main/java/overcloud/blog/usecase/article/get_article/ArticleResponse.java
+++ src/main/java/overcloud/blog/usecase/article/get_article/ArticleResponse.java
@@ -1,4 +1,24 @@
 package overcloud.blog.usecase.article.get_article;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ArticleResponse {
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("body")
+    private String body;
+
+    @JsonProperty("createdAt")
+    private String createdAt;
 }
--- src/main/java/overcloud/blog/usecase/article/get_article/GetArticleImpl.java
+++ src/main/java/overcloud/blog/usecase/article/get_article/GetArticleImpl.java
@@ -1,4 +1,34 @@
 package overcloud.blog.usecase.article.get_article;
 
-public class GetArticleImpl {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import overcloud.blog.entity.ArticleEntity;
+import overcloud.blog.repository.ArticleRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class GetArticleImpl implements GetArticle {
+    private final ArticleRepository articleRepository;
+
+    public GetArticleImpl(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @Override
+    @Transactional
+    public ArticleResponse getArticle(String articleId) {
+        Optional<ArticleEntity> articleEntityOptional = articleRepository.findById(UUID.fromString(articleId));
+
+        if (articleEntityOptional.isEmpty()) {
+
+        }
+
+
+        return toArticleResponse(articleEntityOptional.get());
+    }
+
+    private ArticleResponse toArticleResponse(ArticleEntity articleEntity) {
+        
+    }
 }

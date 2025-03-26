3df40eedc7824106a3ae48f1062f6990d31898cc
shinymilotic <trungtin.mai1412@gmail.com>
2025-03-26 15:54:38 +0700

    update

--- src/main/java/overcloud/blog/usecase/article/get_article/GetArticleImpl.java
+++ src/main/java/overcloud/blog/usecase/article/get_article/GetArticleImpl.java
@@ -4,15 +4,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import overcloud.blog.entity.ArticleEntity;
 import overcloud.blog.repository.ArticleRepository;
+import overcloud.blog.utils.validation.ObjectsValidator;
+
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class GetArticleImpl implements GetArticle {
     private final ArticleRepository articleRepository;
+    private final ObjectsValidator validator;
 
-    public GetArticleImpl(ArticleRepository articleRepository) {
+    public GetArticleImpl(ArticleRepository articleRepository,
+                          ObjectsValidator validator) {
         this.articleRepository = articleRepository;
+        this.validator = validator;
     }
 
     @Override
@@ -20,15 +25,16 @@ public class GetArticleImpl implements GetArticle {
     public ArticleResponse getArticle(String articleId) {
         Optional<ArticleEntity> articleEntityOptional = articleRepository.findById(UUID.fromString(articleId));
 
-        if (articleEntityOptional.isEmpty()) {
-
-        }
-
-
-        return toArticleResponse(articleEntityOptional.get());
+        return articleEntityOptional.map(this::toArticleResponse).orElse(null);
     }
 
     private ArticleResponse toArticleResponse(ArticleEntity articleEntity) {
-        
+        ArticleResponse response = new ArticleResponse();
+        response.setId(articleEntity.getArticleId().toString());
+        response.setTitle(articleEntity.getTitle());
+        response.setDescription(articleEntity.getDescription());
+        response.setBody(articleEntity.getBody());
+        response.setCreatedAt(articleEntity.getCreatedAt().toString());
+        return response;
     }
 }
